2638b1eb11266cd117672183b028bcb3e9143f08
the3dsandwich <duhweiwei1013@gmail.com>
2018-03-17 23:02:58 +0800

    save session for changing error message when quantity exceedes what's allowed

--- app/component/student/playerProfile.jsx
+++ app/component/student/playerProfile.jsx
@@ -9,18 +9,26 @@ import Switch from 'material-ui/Switch';
 import { FormControlLabel } from 'material-ui/Form';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
+import Modal from 'material-ui/Modal';
 import EndGame from './end-game';
 const styles = theme => ({
-  root: theme.mixins.gutters({
-    paddingTop: 16,
-    paddingBottom: 16,
-    marginTop: theme.spacing.unit * 3,
-    marginBottom: theme.spacing.unit * 3,
-  }),
-  fullWidthButton: theme.mixins.gutters({
-        marginTop: theme.spacing.unit,
-        width: '100%'
-      }),
+    root: theme.mixins.gutters({
+        paddingTop: 16,
+        paddingBottom: 16,
+        marginTop: theme.spacing.unit * 3,
+        marginBottom: theme.spacing.unit * 3,
+    }),
+    fullWidthButton: theme.mixins.gutters({
+            marginTop: theme.spacing.unit,
+            width: '100%'
+        }),
+    paper: {
+        position: 'absolute',
+        width: theme.spacing.unit * 50,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing.unit * 4,
+    },
 });
 class PlayerProfile extends React.Component {  
   constructor(props){
@@ -31,6 +39,7 @@ class PlayerProfile extends React.Component {
           borrowing: 0,
           endSession: false,
           open: false,
+          modalOpen: false,
           snackbarMessage: ''
       };
       this.renderSnackBar = () => {
@@ -152,6 +161,16 @@ class PlayerProfile extends React.Component {
     this.setState({
         submit: true
     })
+    if( quantity > companyInfo.maximum || quantity < companyInfo.minimum ) {
+        // alert
+        this.setState({
+            quantity: null,
+            modalOpen: true,
+        });
+        console.log("quantity not allowed");
+        // alert
+        return 0;
+    }
     var forSend = {
         submit: true,
         isBorrowing: checkedBorrowing,
@@ -177,8 +196,33 @@ class PlayerProfile extends React.Component {
         }
     })
 }   
-  addMessageToDatabase(message){
-      this.database.database().ref(this.roomNum).child('on').child('console').push().set(message)
+
+    handleModalClose(){
+        this.setState({modalOpen: false});
+    }
+
+    addMessageToDatabase(message){
+        this.database.database().ref(this.roomNum).child('on').child('console').push().set(message)
+    }
+
+  renderErrorModal(){
+    let that = this;
+    return(<div>
+    <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={that.state.modalOpen}
+        onClose={that.handleModalClose}>
+            <div className={that.classes.paper}>
+                <Typography variant="title" id="modal-title">
+                    Oopsy
+                </Typography>
+                <Typography variant="subheading" id="simple-modal-description">
+                    It seems that your quantity is not what it should be. Check the maximum and minimum amount you can input! 
+                </Typography>
+            </div>
+    </Modal></div>
+    )
   }
   
   renderAsset(){
@@ -282,8 +326,9 @@ class PlayerProfile extends React.Component {
     let {companyInfo} = this.state;
     return (
     <div>
+    {/* {this.renderErrorModal()} */}
     <Typography type="display1" gutterBottom>{'Name: ' + companyInfo.companyName}</Typography>
-
+    
     <Grid container 
     alignItems = 'flex-start'
     justify = 'flex-start'
@@ -323,12 +368,12 @@ class PlayerProfile extends React.Component {
                     <Paper className={classes.root} elevation={4}>
                     <Table className={classes.table}>
                     <TableRow>
-                        <TableCell style ={{width: '40%'}}>Minimum</TableCell>
-                        <TableCell >{companyInfo.minimum}</TableCell>
-                    </TableRow>
-                    <TableRow>
                         <TableCell style ={{width: '40%'}}>Maximum</TableCell>
                         <TableCell >{companyInfo.maximum}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell style ={{width: '40%'}}>Minimum</TableCell>
+                        <TableCell >{companyInfo.minimum}</TableCell>
                     </TableRow>
                     </Table>
                     </Paper>
